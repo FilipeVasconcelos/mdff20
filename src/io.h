ce8ec3ac02288163ee79c65b20359416cca81544
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-15 16:31:54 +0200

    some macros for stdout

--- src/io.h
+++ src/io.h
@@ -1,7 +1,12 @@
 #ifndef IO_H
 #define IO_H
 #include <stdbool.h>
-#define SEPARATOR for(int i=0;i<61;i++) putchar('=');putchar('\n') 
+
+
+#define SEPARATOR  for(int i=0;i<61;i++) putchar('=');putchar('\n') 
+#define LSEPARATOR for(int i=0;i<61;i++) putchar('-');putchar('\n') 
+#define BLANKLINE  putchar('\n');
+
 #define io_node if ( ionode ) 
 
 char *controlfn; 
