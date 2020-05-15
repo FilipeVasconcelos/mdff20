724cdf7f029d7d7c0baf6096f035fff93c01520b
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-15 19:00:41 +0200

    control.s formatting

---
+++ src/control.s
@@ -0,0 +1,16 @@
+# ------------
+#   field
+# ------------
+lnmlj
+mass 39.45
+cutshortrange 8.5 
+skindiff .1
+# ------------
+#    md  
+# ------------
+temp 90.0
+npas 0
+nprint 100 
+nequil 100
+dt .001
+tauTberendsen .001
