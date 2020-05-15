4be5a285aa5b1016c8a3fc29ccd5e7fe3891463c
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-15 23:39:38 +0200

    read esplj sigmalj and othersnmlj  parameters from control file

--- src/nmlj.h
+++ src/nmlj.h
@@ -2,23 +2,24 @@
 #define NMLJ_H
 #define NTYPEMAX 16
 
-double qlj    [NTYPEMAX][NTYPEMAX] ;
-double plj    [NTYPEMAX][NTYPEMAX] ;
-double epslj  [NTYPEMAX][NTYPEMAX] ;
-double sigmalj [NTYPEMAX][NTYPEMAX] ;
-//double rcutsq [NTYPEMAX][NTYPEMAX] ;
-double rcutsq ;
-double epsp   [NTYPEMAX][NTYPEMAX] ;
+bool   lsymmetric                   ;
+double qlj     [NTYPEMAX][NTYPEMAX] ;
+double plj     [NTYPEMAX][NTYPEMAX] ;
 double epslj   [NTYPEMAX][NTYPEMAX] ;
-double sigsq  [NTYPEMAX][NTYPEMAX] ;
-double fc     [NTYPEMAX][NTYPEMAX] ;
+double sigmalj [NTYPEMAX][NTYPEMAX] ;
+double rcutsq                       ;
+double epsp    [NTYPEMAX][NTYPEMAX] ;
+double epslj   [NTYPEMAX][NTYPEMAX] ;
+double sigsq   [NTYPEMAX][NTYPEMAX] ;
+double fc      [NTYPEMAX][NTYPEMAX] ;
 
 double qtwo    [NTYPEMAX][NTYPEMAX] ;
-double ptwo   [NTYPEMAX][NTYPEMAX] ;
+double ptwo    [NTYPEMAX][NTYPEMAX] ;
 
-char trunclabel[3][MAX_LEN+1];
+int  trunctype                         ;  
+char trunclabel[3][MAX_LEN+1]       ;
 
-void init_nmlj();
-void info_nmlj();
+void init_nmlj()                    ;
+void info_nmlj()                    ;
 void engforce_nmlj_pbc();
 #endif
