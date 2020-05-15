360fa440b3d1f965fbd6b0ef8ddc2ea0a8859ff3
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-15 16:34:23 +0200

    info_field;read_field update

--- src/field.c
+++ src/field.c
@@ -11,45 +11,48 @@ int read_field(char* controlfn)
    char buffer[MAX_LEN+1];
    double data;
    FILE * fp;
-
    fp = fopen (controlfn, "r");
-
-   if (NULL == fp ) 
-   {
+   if (NULL == fp )  {
        perror("opening database file");
        return (-1);
    }
-
-   while (EOF != fscanf(fp, "%s %lf\n", buffer,&data))
-   {
-        if (strcmp(buffer,"cutshortrange") == 0 )
-        {
+   while ( (EOF != fscanf(fp, "%s %lf\n", buffer,&data)) ||
+           (EOF != fscanf(fp, "%s\n", buffer)) )  {
+        if (strcmp(buffer,"cutshortrange") == 0 ) {
             cutshortrange=data;
-            printf("cutshortrange   : %.2f\n", cutshortrange );
         } 
-        if (strcmp(buffer,"skindiff") == 0 )
-        {
+        if (strcmp(buffer,"trunctype") == 0 ) {
+            trunctype=data;
+        } 
+        if (strcmp(buffer,"skindiff") == 0 ) {
             skindiff=data;
-            printf("skindiff        : %.2f\n", skindiff );
         } 
-        if (strcmp(buffer,"mass") == 0 )
-        {
+        if (strcmp(buffer,"mass") == 0 ) {
             mass[0]=data;
-            printf("skindiff        : %.2f\n", skindiff );
+        } 
+        if (strcmp(buffer,"lnmlj") == 0 ) {
+            lnmlj=true;
         } 
    }
-
    fclose(fp);
-   
    return(0);
+}
+
+void info_field(){
+
+}
+
+void init_field(char* controlfn){
+    read_field(controlfn);
+    init_nmlj();
 }
 
 
 void engforce()
 {
     double ulj;
-
     engforce_nmlj_pbc(&ulj);
-
     u_lj=ulj;
 }
+
+
