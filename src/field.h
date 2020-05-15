bf49b16e695cf4cd693b9a8a03bb46475f797729
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-15 16:33:38 +0200

    some global field variables : lnmlj,trunctype and prototypes : init_field,info_field

--- src/field.h
+++ src/field.h
@@ -1,8 +1,15 @@
 #ifndef FIELD_H
 #define FIELD_H
+#include <stdbool.h>
+bool lnmlj;
 double cutshortrange;
 double skindiff;
+int trunctype;
+
+
 
 int read_field(char* controlfn);
+void init_field(char* controlfn);
+void info_field();
 void engforce();
 #endif
