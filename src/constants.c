9d7e4686d3f4bc9be8a53cd1489037284ddf3e39
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-15 14:54:24 +0200

    constant radian should be fixed not 'calculated'

--- src/constants.c
+++ src/constants.c
@@ -8,6 +8,10 @@ void gen_constants()
     boltz_unit = 8.61734229648141E-05;  // boltzmann constant ( energy in eV ) 
     time_unit  = 98.2269514139276    ;  // unit of time picosecond => angstrom * ( atomicmassunit / eV ) ** 0.5
     press_unit = 0.00624150964712042;    // GPa => internal unit of pressure (eV/A^3)
+
+
+    radian = 180. / PI;     
+
     for (int it=0;it<NTYPEMAX;it++)
     {
         atypei[it] = malloc((MAX_LEN+1)*sizeof (char*));
