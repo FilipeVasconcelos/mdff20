4e3c8a57088ec9180c88246617679f3e10534262
Filipe Vasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-16 19:25:50 +0200

    read_posff is cleaner!

--- src/read_posff.c
+++ src/read_posff.c
@@ -11,13 +11,10 @@
 #include "cell.h"
 #include "io.h"
 
-int read_posff(CELL *Cell)
+int read_posff()
 {
 
-    char buffer[MAX_LEN+1];
     char cpos[MAX_LEN+1];
-    int data;
-    double f1,f2,f3,f4,f5,f6,f7,f8,f9;
     FILE * fp;
 
     fp = fopen ("POSFF","r");
@@ -34,40 +31,33 @@ int read_posff(CELL *Cell)
     
     // reading nion number of ions in POSFF 
     fscanf(fp, "%d", &nion);
-
     // reading config name
-    fscanf(fp, "%s", buffer);
-    strcpy(configname,buffer); 
-
+    fscanf(fp, "%s", configname);
     // reading cell parameters
-    fscanf(fp, "%lf %lf %lf\n",&f1,&f2,&f3);
-    fscanf(fp, "%lf %lf %lf\n",&f4,&f5,&f6);
-    fscanf(fp, "%lf %lf %lf\n",&f7,&f8,&f9);
-    Cell->A[0][0]=f1;   Cell->A[0][1]=f2;   Cell->A[0][2]=f3;
-    Cell->A[1][0]=f4;   Cell->A[1][1]=f5;   Cell->A[1][2]=f6;
-    Cell->A[2][0]=f7;   Cell->A[2][1]=f8;   Cell->A[2][2]=f9;
-    lattice(Cell);
-    rhoN=(double)nion * Cell->inveOmega;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            fscanf(fp, "%lf",&simuCell.A[i][j]);
+        }
+    }
+    lattice(&simuCell);
+    rhoN=(double)nion * simuCell.inveOmega;
 
     // ------------------
     // type informations 
     // ------------------
-    //
     //ntype : number of types in POSFF
     fscanf(fp, "%d", &ntype); //ntype
 
     //atypei : ion types char 
     io_node printf("found type information on POSFF :");
     for (int it=0;it<ntype;it++) {
-        fscanf(fp,"%s",buffer);
-        strcpy(atypei[it],buffer);
+        fscanf(fp,"%s",atypei[it]);
         io_node printf("%5s",atypei[it]);
     }
     //natmi : ions per type
     io_node printf("\n                                 ");
     for (int it=0;it<ntype;it++) {
-        fscanf(fp,"%d",&data);
-        natmi[it]=data;
+        fscanf(fp,"%d",&natmi[it]);
         io_node printf("%5d",natmi[it]);
     }
     io_node putchar('\n');
@@ -76,17 +66,17 @@ int read_posff(CELL *Cell)
     alloc_config();
 
     // Direct or Cartesian
-    fscanf(fp, "%s", buffer);
-    strcpy(cpos,buffer);
+    fscanf(fp, "%s", cpos);
+    //strcpy(cpos,buffer);
+
     // reading positions of ions 
     for (int ia=0;ia<nion;ia++) {
-        fscanf(fp,"%s %lf %lf %lf",buffer,&f1,&f2,&f3);
-        strcpy(atype[ia],buffer);
-        rx[ia]=f1;ry[ia]=f2;rz[ia]=f3;
+        fscanf(fp,"%s %lf %lf %lf",atype[ia],&rx[ia],&ry[ia],&rz[ia]);
     }
+    // if position are in direct coordinates => cartesian coordinates
     if ((strcmp(cpos,"Direct") == 0 ) || (strcmp(cpos,"D") == 0 )) {
         io_node printf("\natomic positions in Direct coordinates\n");
-        dirkar(nion, rx, ry, rz , Cell->A);
+        dirkar(nion, rx, ry, rz , simuCell.A);
     }
     else{
         io_node printf("\natomic positions in Cartesian coordinates\n");
