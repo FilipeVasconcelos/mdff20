1fec309bf896dc984a18b1ab5f1f1c1ed2e17ab4
Filipe Vasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-18 09:50:36 +0200

    blankline removed in source code

--- src/io.h
+++ src/io.h
@@ -6,15 +6,11 @@
 #define SEPARATOR  for(int i=0;i<61;i++) putchar('=');putchar('\n') 
 #define LSEPARATOR for(int i=0;i<61;i++) putchar('-');putchar('\n') 
 #define BLANKLINE  putchar('\n');
-
 #define io_node if ( ionode ) 
-
 #define pError(X) if (ionode) fprintf(stderr,RED"ERROR : "RES X)
-
 
 char *controlfn; 
 bool ionode;
-
 
 void headerstdout(char* time ,int numprocs);
 void init_io();
