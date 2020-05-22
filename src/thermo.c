a98a0be3248b56c2cca17b38cdcecb71458c1d65
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-22 16:54:59 +0200

    using format constant EE

--- src/thermo.c
+++ src/thermo.c
@@ -22,29 +22,29 @@ void info_thermo(){
         printf("  Thermodynamic information                    \n" );
         printf("  ---------------------------------------------\n" );
         printf("  step                  = %9d\n"             ,istep);
-        printf("  time                  = %19.12e\n"          ,time);
-        printf("  Ekin                  = %19.12e\n"         ,e_kin);
-        printf("  Temp                  = %19.12e\n"        ,temp_r);
-        printf("  U_lj                  = %19.12e\n"          ,u_lj);
-        printf("  Pressure              = %19.12e\n"      ,pressure);
-        printf("  Pvir_lj               = %19.12e\n"       ,pvir_lj);
-        printf("  volume                = %19.12e\n",simuCell.Omega);
-        printf("  a cell                = %19.12e\n"         ,acell);
-        printf("  b cell                = %19.12e\n"         ,bcell);
-        printf("  c cell                = %19.12e\n"         ,ccell);
+        printf("  time                  = "EE"\n"             ,time);
+        printf("  Ekin                  = "EE"\n"            ,e_kin);
+        printf("  Temp                  = "EE"\n"           ,temp_r);
+        printf("  U_lj                  = "EE"\n"             ,u_lj);
+        printf("  Pressure              = "EE"\n"         ,pressure);
+        printf("  Pvir_lj               = "EE"\n"          ,pvir_lj);
+        printf("  volume                = "EE"\n",   simuCell.Omega);
+        printf("  a cell                = "EE"\n"            ,acell);
+        printf("  b cell                = "EE"\n"            ,bcell);
+        printf("  c cell                = "EE"\n"            ,ccell);
         printf("  ---------------------------------------------\n" );
-        printf("  Etot                  = %19.12e\n"         ,e_tot);
-        printf("  Htot                  = %19.12e\n"         ,e_tot);
+        printf("  Etot                  = "EE"\n"            ,e_tot);
+        printf("  Htot                  = "EE"\n"            ,e_tot);
         printf("\n");
         printf("  ---------------------------------------------\n" );
         printf("  non_bonded stress tensor :\n"                    );
         printf("  ---------------------------------------------\n" );
         for (int i=0;i<3;i++){
-            printf("  %14.7e %14.7e %14.7e\n",tau_nonb[i][0],tau_nonb[i][1],tau_nonb[i][2]);
+            printf("  "ee3"\n",tau_nonb[i][0],tau_nonb[i][1],tau_nonb[i][2]);
             iso+=tau_nonb[i][i];
         }
         printf("  ---------------------------------------------\n" );
-        printf("  iso = %15.7e (%15.7e)\n",iso*onethird,iso*onethirdnion);
+        printf("  iso = "ee "("ee")\n",iso*onethird,iso*onethirdnion);
         putchar('\n');
     }
 }
