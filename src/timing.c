986d3412759a5eeeabcecce9a3ff73e5b48f27cc
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-22 16:22:15 +0200

    more timing measurement:  verlet, propagator; cleaning output timing info

--- src/timing.c
+++ src/timing.c
@@ -2,6 +2,9 @@
 #ifdef MPI
 #include <mpi.h>
 #endif
+#ifdef OMP 
+#include <omp.h>
+#endif
 #include <time.h>
 #include "timing.h"
 #include "io.h"
@@ -9,6 +12,9 @@
 void statime(int x){
 #ifdef MPI
     ttt[x]=MPI_Wtime();
+#elif OMP
+//    printf("omp_get_wtime\n");
+    ttt[x]=omp_get_wtime();
 #else
     ttt[x]=clock();
 #endif
@@ -17,14 +23,18 @@ void mestime(double *whole,int from,int to){
     *whole+=ttt[from]-ttt[to];
 }
 void writime(char* label,int step,int from,int to){
-#ifdef MPI
+#if defined(MPI) || defined(OMP)
     io_node printf("  step : %-6d  %-6s cpu time : %8.3f (s)\n",step,label,ttt[from]-ttt[to]);
 #else
-    io_node printf("  step : %-6d  %-6s cpu time : %8.3f (s)\n",step,label,(double)(ttt[from]-ttt[to])/CLOCKS_PER_SEC);
+    io_node printf("  step : %-6d  %-6s cpu time : %8.3f (s)\n",step,label,(((double)ttt[from]-ttt[to])/CLOCKS_PER_SEC));
 #endif
 }
 void writimewhole(char* label, double whole){
+#if defined(MPI) || defined(OMP)
     printf("  %-20s  : %12.3f\n",label,whole);
+#else
+    printf("  %-20s  : %12.3f\n",label,(double) (whole/CLOCKS_PER_SEC));
+#endif
 }
 
 void info_timing(){
@@ -32,11 +42,13 @@ void info_timing(){
     if (ionode){
         BLANKLINE;
         LSEPARATOR;
-        printf("print timing info\n");
+        printf("  Timing Info  \n");
         LSEPARATOR;
         writimewhole("MD",mdstepCPUtime);
-        writimewhole("engforce",engforceCPUtime);
-        writimewhole("dir->kar & kar->dir",dirkardirCPUtime);
+        writimewhole("Engforce",engforceCPUtime);
+        writimewhole("Verlet List",verletLCPUtime);
+        writimewhole("Integration ",propagatorCPUtime);
+        writimewhole("Dir<->Kar",dirkardirCPUtime);
         writimewhole("MPI communication",COMMCPUtime);
         writimewhole("test measure",CPUtime);
     }
--- src/timing.h
+++ src/timing.h
@@ -1,13 +1,15 @@
 #ifndef TIMING_H
 #define TIMING_H
 #define MAX_INDEX_TIME 16 
-// 0 -> 1 MD step
-// 2 -> 3 engforce
-// 4 -> 5 dirkardir
-// 6 -> 7 test measure
-// 8 -> 9 COMM
+// 0 -> 1   MD step
+// 2 -> 3   engforce
+// 4 -> 5   dirkardir
+// 6 -> 7   test measure
+// 8 -> 9   COMM
+// 10 -> 11 verlet
+// 12 -> 13 propagator/integration
 #include <time.h>
-#ifdef MPI
+#if defined(MPI) || defined(OMP)
 double ttt[MAX_INDEX_TIME]; /* ttt[0] reference time */
 #else
 clock_t ttt[MAX_INDEX_TIME]; /* ttt[0] reference time */
@@ -17,6 +19,8 @@ double engforceCPUtime;
 double dirkardirCPUtime;
 double CPUtime; /* tmp measure */
 double COMMCPUtime; /* tmp measure */
+double verletLCPUtime ;
+double propagatorCPUtime;
 
 
 void info_timing();
