594f5bd4e010cd942f5239ce718e99103ec4cd4f
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-22 17:02:55 +0200

    add iopnode function (printing output node)

--- src/io.c
+++ src/io.c
@@ -8,6 +8,7 @@
 #endif
 #include "io.h"
 
+/******************************************************************************/
 void headerstdout(char *starting_time, int numprocs){
 
     char *username = getenv("USER");
@@ -17,29 +18,30 @@ void headerstdout(char *starting_time, int numprocs){
     uname(&hostname);
 
     if (ionode) {
-
-    printf("                            \\\\|//                          \n");
-    printf("                           -(o o)-                           \n");
-    printf("========================oOO==(_)==OOo========================\n");
-    printf("     ____    ____ ______  ________ ________                  \n");
-    printf("    |_   \\  /   _|_   _ `|_   __  |_   __  |                \n");
-    printf("      |   \\/   |   | | `. \\| |_ \\_| | |_ \\_|____    ____ \n");
-    printf("      | |\\  /| |   | |  | ||  _|    |  _| / ___ `..'    '.  \n");
-    printf("     _| |_\\/_| |_ _| |_.' _| |_    _| |_ |_/___) |  .--.  | \n");
-    printf("    |_____||_____|______.|_____|  |_____| .'____.| |    | |  \n");
-    printf("                                         / /_____|  `--'  |  \n");
-    printf("                                         |_______|'.____.'   \n");
-    printf("\n");
-    SEPARATOR;
-    printf("MOLECULAR DYNAMICS ...for fun in 2020 ... but now in C ;)    \n");
-    printf("filipe.manuel.vasconcelos@gmail.com                          \n");
-    printf("Running on : %d node%s\n",numprocs,plural);
-    printf("by user    : %s\n",username);
-    printf("host       : %s %s %s\n",hostname.nodename,hostname.sysname,hostname.machine);
-    printf("Date       : %s", starting_time);
+      printf("                            \\\\|//                          \n");
+      printf("                           -(o o)-                           \n");
+      printf("========================oOO==(_)==OOo========================\n");
+      printf("     ____    ____ ______  ________ ________                  \n");
+      printf("    |_   \\  /   _|_   _ `|_   __  |_   __  |                \n");
+      printf("      |   \\/   |   | | `. \\| |_ \\_| | |_ \\_|____    ____ \n");
+      printf("      | |\\  /| |   | |  | ||  _|    |  _| / ___ `..'    '.  \n");
+      printf("     _| |_\\/_| |_ _| |_.' _| |_    _| |_ |_/___) |  .--.  | \n");
+      printf("    |_____||_____|______.|_____|  |_____| .'____.| |    | |  \n");
+      printf("                                         / /_____|  `--'  |  \n");
+      printf("                                         |_______|'.____.'   \n");
+      printf("\n");
+      SEPARATOR;
+      printf("MOLECULAR DYNAMICS ...for fun in 2020 ... but now in C ;)    \n");
+      printf("filipe.manuel.vasconcelos@gmail.com                          \n");
+      printf("Running on : %d node%s\n",numprocs,plural);
+      printf("by user    : %s\n",username);
+      printf("host       : %s %s %s\n",hostname.nodename,
+                                       hostname.sysname,
+                                       hostname.machine);
+      printf("Date       : %s", starting_time);
     }
 }
-
+/******************************************************************************/
 void init_io(){
     int myrank =0;
 #ifdef MPI
@@ -47,30 +49,18 @@ void init_io(){
     MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
 #endif
-
     if (myrank==0) {
         ionode = true;
     }
     else{
         ionode = false;
     }
-
 }
 
-int check_FTstring(char* label,char buffer[MAX_LEN+1] ){
-    int check=-1;
-    for (int k=0;k<8;k++){
-        if (strcmp(buffer,allowed_Tstring[k]) == 0){
-            check=1;
-        }
-        if (strcmp(buffer,allowed_Fstring[k]) == 0){
-            check=0;
-        }
-    }
-    if (check<0){
-        pError("value not allowed in input file\n");
-        printf("%s %s\n",label,buffer);
-        exit(-1);
-    }
-    return check;
+/******************************************************************************/
+int iopnode(int step,int npas,int nprint){
+//    printf("step : %d %d %d %d\n",step,npas,nprint,step%nprint==0);
+    return ( (step % nprint==0 || step == npas ) && (step > 0) );
 }
+
+
--- src/io.h
+++ src/io.h
@@ -7,7 +7,7 @@
 #define LSEPARATOR for(int i=0;i<61;i++) putchar('-');putchar('\n') 
 #define BLANKLINE  putchar('\n');
 #define io_node if ( ionode ) 
-#define io_pnode if ( (ionode) && (istep % nprint == 0) ) 
+#define io_pnode if ( iopnode(istep,npas,nprint) ) 
 #define pError(X) if (ionode) fprintf(stderr,RED"ERROR : "RES X)
 
 char *controlfn; 
@@ -15,5 +15,6 @@ bool ionode;
 
 void headerstdout(char* time ,int numprocs);
 void init_io();
+int iopnode(int step,int npas, int nprint);
 int check_FTstring(char* label,char buffer[MAX_LEN+1] );
 #endif
