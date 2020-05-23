b6a8d9a001904f9cba41dc0fda7607a9287d4a9d
Filipe Vasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-05-23 19:26:31 +0200

    ex0 is ready

---
+++ examples/ex0/config/control.s
@@ -0,0 +1,26 @@
+# Example 0: Minimum settings
+# ============================
+# main parameters
+# ============================
+lnmlj   true
+cutshortrange 10.0 
+lverletL  true
+trunc no
+skindiff 1.5
+# ============================
+# md parameters 
+# ============================
+integrator nve-vv
+temp 10.0
+npas 0
+dt .001
+tauTberendsen .001
+nprint 0
+fprint 0
+nequil 0
+# ============================
+# parameters for the force-field 
+# ============================
+massit 39.95
+epslj   .010323576
+sigmalj 3.405
---
+++ examples/ex0/run_ex0.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# files :
+#  		  config/control.s  (MDFF)
+#		  config/POSFF      (MDFF)
+#		  config/CONFIG     (DLPOLY)
+#		  config/CONTROL    (DLPOLY)
+#		  config/FIELD      (DLPOLY)
+#                 config/input.cp2k (CP2K)
+#
+# ====================================================
+# user settings
+
+EXECP2K=cp2k
+EXEDLPOLY=/home/filipe/build/dl_poly/execute/DLPOLY.X
+EXEMDFF=mdff20.x
+CONTROLFF=control.s
+
+do_cp2k=true
+do_dlpoly=true
+
+# =====================================================
+
+sep="=================================================="
+echo $sep
+echo "# Example 0: Minimum settings with FCC structure"
+echo "# FCC LJ a=5.24 A (in reduced units 1.5414)"
+echo "# natm = 256"
+echo "# more details in ${CONTROLFF} file"
+
+rm -rf mdff/
+mkdir mdff
+cd mdff
+echo 
+echo "# mdff calculation "
+cp ../config/${CONTROLFF} .
+cp ../config/POSFF .
+$EXEMDFF ${CONTROLFF} > stdout 
+cd ..
+
+if $do_dlpoly; then
+	echo "# dlpoly calculation requested "
+	rm -rf dl_poly
+	mkdir dl_poly 
+	cd dl_poly
+	cp ../config/CONTROL .
+	cp ../config/FIELD .
+	cp ../config/CONFIG .
+	$EXEDLPOLY 
+	cd ..
+fi
+
+if $do_cp2k; then
+	echo "# cp2k calculation requested "
+	rm -rf cp2k 
+	mkdir cp2k
+	cd cp2k
+	cp ../config/input.cp2k .
+	$EXECP2K -i input.cp2k -o output.cp2k
+	cd ..
+fi
+
+
+echo
+echo $sep
+echo 
+# print energy informations
+etot=$(grep Etot mdff/stdout | tail -n 1 | awk '{print $NF}')
+etot2=$(echo ${etot} | sed -e 's/[eE]+*/\*10\^/')
+nion=$(grep nion mdff/stdout | awk '{print $NF}')
+lr=$(grep "long range correction (energy)" mdff/stdout |  awk '{print $NF}')
+lr2=$(echo ${lr} | sed -e 's/[eE]+*/\*10\^/')
+etot3=$(echo "scale=8;${etot2}" | bc -l | awk '{printf("%.4f\n", $1)}')
+etotlr=$(echo "scale=8;(${etot2})+(${lr2})" | bc -l | awk '{printf("%.4f\n", $1)}')
+echo "Etot (mdff)                  = " ${etot3}
+echo "Etot + long range correction = " ${etotlr}
+echo ""
+
+
+if [ $do_cp2k = "true" ] && [ -f cp2k/FCC_LJ-1.ener ]
+then
+	etotcp2k=`tail -n 1 cp2k/FCC_LJ-1.ener | awk '{printf ("%.4f\n", $5*27.211385)}'`
+	echo "cp2k                         = " $etotcp2k 
+fi
+if [ $do_dlpoly = "true" ] && [ -f dl_poly/STATIS ]
+then
+	etotdl=`head -n 4 dl_poly/STATIS | tail -n 1 | awk '{printf("%.4f\n", $1*1.0)}'`
+	echo "dl_poly                      = " $etotdl 
+fi
+
+
+exit 0
