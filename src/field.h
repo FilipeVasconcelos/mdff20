8f7b13fb46af9eb4dd97019f3edd76d1b75a3ff6
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-06-09 19:16:34 +0200

    clean code

--- src/field.h
+++ src/field.h
@@ -1,20 +1,31 @@
 #ifndef FIELD_H
 #define FIELD_H
 #include <stdbool.h>
-bool lnonbonded; /* non bonded pair potential nmlj bhmftd */
-bool lsymmetric; /* are those non bonded potentials symmetric */
-bool      lnmlj; /* n-m lennard-jones potential */
-bool     lbhmft; /* Born Huggins-Meyer potential + Fumi-Tossi */
-bool    lbhmftd; /* Born Huggins-Meyer potential + Fumi-Tossi + Damping */ 
-bool lcoulombic; /* Coulombic/Electrostatic potential */
+#include "constants.h"
+bool       lnonbonded; /* non bonded pair potential nmlj bhmftd */
+bool       lsymmetric; /* are those non bonded potentials symmetric */
+bool            lnmlj; /* n-m lennard-jones potential */
+bool           lbhmft; /* Born Huggins-Meyer potential + Fumi-Tossi */
+bool          lbhmftd; /* Born Huggins-Meyer potential + Fumi-Tossi + Damping */ 
+bool       lcoulombic; /* Coulombic/Electrostatic potential */
+bool lpolar[NTYPEMAX]; /* induced moment from polarizability for type */ 
+/* ****************************************************************************/
+/*                       EWALD SUMMATION PARAMETERS                           */
+/* ****************************************************************************/
+bool          lautoES; /* auto-determination of Ewald parameter from epsw ( accuracy) */
+double           epsw; /* accuracy of the ewald sum */
+double        alphaES; /*Ewald sum */
+int            kES[3]; /* kmax of ewald sum in reciprocal space */
 
 
-bool    lautoES; /* auto-determination of Ewald parameter from epsw ( accuracy) */
-double     epsw; /* accuracy of the ewald sum */
-double  alphaES; /*Ewald sum */
-int      kES[3]; /* kmax of ewald sum in reciprocal space */
-
-
+/* ****************************************************************************/
+/*                              global parameters                             */
+/* ****************************************************************************/
+double        srcutsq; /* shortrange cut ^2 */
+double        lrcutsq; /* longrange cut ^2 */
+/* ****************************************************************************/
+/*                                prototypes                                  */
+/* ****************************************************************************/
 int read_field(char* controlfn);
 void init_field(char* controlfn);
 void info_field();
