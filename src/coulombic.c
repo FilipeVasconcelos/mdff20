d55779bde4f9a4ffd154a418747d6bd7c270657c
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-06-09 23:51:55 +0200

    preparing induced and damping PIM calculation

--- src/coulombic.c
+++ src/coulombic.c
@@ -5,7 +5,74 @@
 #include "config.h"
 #include "coulombic.h"
 #include "kspace.h"
+#include "pim.h"
 
+/******************************************************************************/
+void allocate_coulombic(){
+    qia=malloc(nion*sizeof(*qia));
+    dipia=malloc(nion*sizeof(*dipia));
+    quadia=malloc(nion*sizeof(*quadia));
+}
+/******************************************************************************/
+void free_coulombic(){
+    free(qia);
+    free(dipia);
+    free(quadia);
+    free_kspace();
+}
+/******************************************************************************/
+void get_dipoles(double (*mu)[3]){
+
+    double (*dipia_ind)[3];
+    dipia_ind=malloc(nion*sizeof(*dipia_ind));
+    switch (algo_pim) {
+        case 0 :
+            momentpolaSCF(dipia_ind);
+           break; 
+        case 1 :
+           break; 
+    }
+
+    free(dipia_ind);
+}
+/******************************************************************************/
+void init_coulombic(){
+    allocate_coulombic();
+    init_kspace();
+    info_coulombic();
+}
+/******************************************************************************/
+void info_coulombic(){
+    if (ionode) {
+        SEPARATOR;
+        printf("coulombic field info\n");
+        LSEPARATOR;
+        printf("coulombic potential\n");
+        LSEPARATOR;
+        putchar('\n');
+        printf("        qi qj   \n");
+        printf(" Vij = -------  \n");
+        printf("         rij    \n");
+        putchar('\n');
+        if (lautoES) {
+            printf("ewald summation parameters (from espw) \n");
+        }
+        else
+        {
+            printf("ewald summation parameters (from user) \n");
+        }
+        printf("alpha                            = %f\n",alphaES);
+        printf("kmax                             = ");
+        for(int k=0;k<3;k++){
+            printf("%d ",kES[k]);
+        }
+        putchar('\n');
+        if (!lautoES) {
+            printf("relative error (user defined)    : "ee"\n",epsw);
+        }
+    };
+}
+/******************************************************************************/
 void sample_field_coulombic(double (*ef)[3],double (*efg)[3][3]){
 
     int mia=10;
@@ -23,50 +90,21 @@ void sample_field_coulombic(double (*ef)[3],double (*efg)[3][3]){
             printf("%5d %5s "ee6"\n",ia,atypia[ia],efg[ia][0][0],efg[ia][1][1],efg[ia][2][2],
                                                    efg[ia][0][1],efg[ia][0][2],efg[ia][0][2]);
         }
-            LSEPARATOR;
-        }
-}
-
-
-void allocate_coulombic(){
-    qia=malloc(nion*sizeof(*qia));
-    dipia=malloc(nion*sizeof(*dipia));
-    quadia=malloc(nion*sizeof(*quadia));
-}
-
-void free_coulombic(){
-    free(qia);
-    free(dipia);
-    free(quadia);
-}
-
-void init_coulombic(){
-    allocate_coulombic();
-    info_coulombic();
-    init_kspace();
-}
-
-void info_coulombic(){
-    if (ionode) {
-        SEPARATOR;
-        printf("coulombic field info\n");
         LSEPARATOR;
-        printf("coulombic potential\n");
-        LSEPARATOR;
-        putchar('\n');
-        printf("        qi qj   \n");
-        printf(" Vij = -------  \n");
-        printf("         rij    \n");
-        putchar('\n');
-        printf("ewald summation parameters\n");
-        printf("alpha                            = %f\n",alphaES);
-        printf("kmax                             = ");
-        for(int k=0;k<3;k++){
-            printf("%d ",kES[k]);
-        }
-        putchar('\n');
-        if (!lautoES) {
-            printf("relative error (user defined)    : "ee"\n",epsw);
-        }
-    };
+    }
 }
+
+/******************************************************************************/
+bool is_induced(){
+    for(int it=0;it<ntype;it++){
+        if ( lpolar[it] )  return true;
+    }
+    return false;
+}
+/******************************************************************************/
+/*bool is_damping(){
+    for(int it=0;it<ntype;it++){
+        if ( lpolar[it] )  return true;
+    }
+    return false    
+}*/
