5c2d6da60b014a609bfe1bac79ba7c511fe4db2b
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-06-09 23:52:23 +0200

    wv

--- src/control.s
+++ src/control.s
@@ -1,42 +1,60 @@
 # ------------
 #   global 
 # ------------
-Fposff 2
+Fposff rnn 
+#lverletL false
 lverletL true 
-cutshortrange 8.5 
-cutlongrange  10.0 
+cutshortrange  5.5 
+cutlongrange   5.0 
 skindiff  1.0 
 lstatic false 
 lreduced false 
+lpstress false 
 # ------------
 #   field
 # ------------
 lcoulombic true 
 lautoES true 
-qit  1.0 
-qit  -1.0
-epsw 1e-6
+epsw 1e-5
 
-lnmlj true 
-massit 39.95 39.95
-
+#--------------
+# NMLJ
+#--------------
+#lnmlj true
 trunc no 
-epslj .010323576
-sigmalj 3.405
+#epslj .010323576
+#sigmalj 3.405
+#--------------
+#BHMFTD
+#--------------
+lbhmft true 
+#massit 39.95
+massit 15.99994 # O
+massit 28.084   # Si
+qit -2.0 #O     
+qit  4.0 #Si 
+
+#         O-O        O-Si  
+#         (eV)       (eV)  
+Abhmftd 7902.1862   1311.5888
+Bbhmftd    4.54668     3.18568
+Cbhmftd   13.145548    0.0
+Dbhmftd   71.279951    0.0
+
 # ------------
 #    md  
 # ------------
-#integrator nvt-nhcn
-integrator nve-vv
-nhc_yosh_order 1      
-nhc_mults      1      
-nhc_n          2 
-timesca_thermo 2.0 
-temp 119.8 
-npas  0
+integrator nvt-nhcn
+#integrator nve-vv
+nhc_n          4 
+nhc_yosh_order 5      
+nhc_mults      4      
+timesca_thermo 1.0
+temp 2500.0 
+npas 1000000
 nprint 1000
-fprint 100
-nequil 20000 
-nequilT 5
-dt .002
-tauTberendsen .002
+fprint 10
+nequil 50000
+nequilT 10
+dt .0005
+tauTberendsen .0005
