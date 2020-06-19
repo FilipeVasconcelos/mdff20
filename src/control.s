c5a46311f32b6aa3609869b3bd0f27c5fa2e0301
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-06-19 17:52:52 +0200

    PIM control

--- src/control.s
+++ src/control.s
@@ -1,7 +1,7 @@
 # ------------
 #   global 
 # ------------
-Fposff rnn 
+Fposff rvf
 #lverletL false
 lverletL true 
 cutshortrange  5.5 
@@ -15,20 +15,20 @@ lpstress false
 # ------------
 lcoulombic true 
 lautoES true 
-epsw 1e-5
+epsw 1e-6
 
 #--------------
 # NMLJ
 #--------------
 #lnmlj true
 trunc no 
+#massit 39.95
 #epslj .010323576
 #sigmalj 3.405
 #--------------
-#BHMFTD
+#  BHMFTD
 #--------------
 lbhmft true 
-#massit 39.95
 massit 15.99994 # O
 massit 28.084   # Si
 qit -2.0 #O     
@@ -40,6 +40,18 @@ Abhmftd 7902.1862   1311.5888
 Bbhmftd    4.54668     3.18568
 Cbhmftd   13.145548    0.0
 Dbhmftd   71.279951    0.0
+# ------------
+#    PIM 
+# ------------
+algo_pim scf
+conv_tol_ind  1e-6    
+algo_extrapolate_dipole aspc
+extrapolate_order 4   
+
+# Oxygen type 
+lpolar true
+polit 1.59150 0.0 0.0 0.0 1.59150 0.0 0.0 0.0 1.59150
+lpolar false 
 
 # ------------
 #    md  
@@ -49,12 +61,11 @@ integrator nvt-nhcn
 nhc_n          4 
 nhc_yosh_order 5      
 nhc_mults      4      
-timesca_thermo 1.0
+timesca_thermo 1.0 
 temp 2500.0 
-npas 1000000
+npas 0 
 nprint 1000
 fprint 10
-nequil 50000
-nequilT 10
+cprint 100000
 dt .0005
 tauTberendsen .0005
