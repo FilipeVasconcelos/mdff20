06c4a94bce5b91c62738d2962f3dc7b83cae451c
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-06-19 17:46:03 +0200

    fonctions principales de PIM

--- src/pim.c
+++ src/pim.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <float.h>
 #include "io.h"
 #include "tools.h"
+#include "field.h"
 #include "pim.h"
+#include "config.h"
+#include "md.h"
+#include "ewald.h"
 
 /******************************************************************************/
 int read_pim (char * controlfn) 
@@ -33,25 +40,43 @@ int read_pim (char * controlfn)
         if (strcmp(buffer,"extrapolate_order") == 0 ) {
             fscanf(fp,"%d",&extrapolate_order);
         } 
+        if (strcmp(buffer,"min_scf_pim_iter") == 0 ) {
+            fscanf(fp,"%d",&min_scf_pim_iter);
+        } 
+        if (strcmp(buffer,"max_scf_pim_iter") == 0 ) {
+            fscanf(fp,"%d",&max_scf_pim_iter);
+        } 
    }
    fclose(fp);
    return 0;
 }
 
-
-void check_pim(){
-
-}
-
 /******************************************************************************/
-void init_pim(char * controlfn){
+void default_pim(){
     /* gen allowed input strings for algoPIM */
     strcpy(allwd_algo_pim[0],"scf");
     strcpy(allwd_algo_pim[1],"scfKO");
+
     /* gen allowed input strings for algo_ext_dipole */
     strcpy(allwd_algo_extrapolate_dipole[0],"poly");
     strcpy(allwd_algo_extrapolate_dipole[1],"aspc");
+    conv_tol_ind  = 1e-6;
+    min_scf_pim_iter = 3;
+    max_scf_pim_iter = 100;
+    extrapolate_order = 0 ;
+    algo_extrapolate_dipole = 0;
+     algo_pim = 0;
+}
 
+/******************************************************************************/
+void check_pim(){
+}
+
+/******************************************************************************/
+void init_pim (char * controlfn) {
+    
+    default_pim();
+    read_pim(controlfn);
     info_pim();
 }
 
@@ -59,13 +84,352 @@ void init_pim(char * controlfn){
 void info_pim(){
     if (ionode) {
         SEPARATOR;
-        printf("pim info \n");
+        printf("pim info\n");
         LSEPARATOR;
         putchar('\n');
+        printf("Polarizable Ion Model\n");
+        switch (algo_pim) {
+            case 0 :
+                printf("SCF algorithm\n");
+                break;
+            case 1 :
+                printf("SCF algorithm KO\n");
+                break;
+        }
+        switch (algo_extrapolate_dipole) {
+            case 0 :
+                printf("polynomial extrapolation\n");
+                break;
+            case 1 :
+                printf("ASPC (Always Stable Predictor Corrector) extrapolation\n");
+                break;
+        }
+        printf("convergence tolerance : %e\n",conv_tol_ind);
     }
 }
 
+/*****************************************************************************
+     ---------------------------------------------------------------
+          u_pol = 1/ ( 2 alpha  ) * | mu_ind | ^ 2
+     ---------------------------------------------------------------
+     note on units :
+        [ poldipia  ] = A ^ 3
+        [ mu_ind ] = e A 
+        [ u_pol  ] = e^2 / A ! electrostatic internal energy  
+*****************************************************************************/
+double get_upol(double (*mu)[3]){
+    double upol=0.0;
+    
+    for(int ia=0;ia<nion;ia++){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                upol+= mu[ia][i]*invepolia[ia][i][j]*mu[ia][j]; 
+            }
+        }
+    }
+    return upol*0.5;
+}
 
-void momentpolaSCF(double (*mu)[3]){
+/******************************************************************************/
+void momentpolaSCF(double (*mu_ind)[3],double *upol){
+
+    printf("inside momentpolaSCF %d %d\n",max_scf_pim_iter,min_scf_pim_iter);
+    int iscf;
+    double rmsd;
+    double (*ef)[3];
+    double (*ef_stat)[3];
+    double (*ef_ind)[3];
+    double u_coul_stat,u_coul_ind,u_coul_pol;
+    double alphaES_save;
+    double uupol;
+    double ppvir;
+    double tau[3][3];
+    double (*efg)[3][3];
+    double (*zeroch);
+
+    zeroch=malloc(nion*sizeof(*zeroch));
+    ef=malloc(nion*sizeof(*ef));
+    efg=malloc(nion*sizeof(*efg));
+    ef_stat=malloc(nion*sizeof(*ef_stat));
+    ef_ind=malloc(nion*sizeof(*ef_ind));
+
+    /* static elecric field charges+dipoles+...*/
+    multipole_ES(qia,dipia,quadia,&u_coul_stat,&ppvir,tau,ef_stat,efg,true,true);
+
+    alphaES_save = alphaES;
+
+    /* first electric field is static */
+    for (int ia=0;ia<nion;ia++){
+        for (int i=0;i<3;i++){
+            ef[ia][i]=ef_stat[ia][i];
+        }
+    }
+    
+
+    /* SCF LOOP */
+    iscf=0;
+    rmsd=DBL_MAX;
+    printf("  ---------------------------------------------\n");
+    printf("             running scf algo                  \n");
+    printf("  ---------------------------------------------\n");
+
+    while ( (iscf<max_scf_pim_iter) && (rmsd>conv_tol_ind)  || (iscf<min_scf_pim_iter) ){
+        printf("iscf %d\n",iscf);
+
+        /* predictor */
+        //extrapolate_dipole_aspc(mu_ind,ef,1);    
+        induced_moment(mu_ind,ef);
+        printf("mu_indx[0] %e efx[0] %e\n",mu_ind[0][0],ef[0][0]);
+
+        uupol=get_upol(mu_ind);
+        printf("uupol %e\n",uupol);
+
+        /* induced electric from only dipoles */
+        multipole_ES(zeroch,mu_ind,quadia,&u_coul_ind,&ppvir,tau,ef_ind,efg,false,true);
+
+        /* ef = ef_stat + ef_ind */
+        for (int ia=0;ia<nion;ia++){
+            for (int i=0;i<3;i++){
+                ef[ia][i] = ef_stat[ia][i] + ef_ind[ia][i];
+            }
+        }
+        printf("efx[0] %e\n",ef[0][0]);
+        u_coul_pol = u_coul_stat - u_coul_ind;
+        printf("u_coul_ind %e u_coul_stat %e u_coul_pol %e\n",u_coul_ind,u_coul_stat,u_coul_pol);
+        /* corrector */
+        extrapolate_dipole_aspc(mu_ind,ef,2);    
+
+        rmsd=get_rmsd_scf(mu_ind, ef);
+        if (ionode) {
+            printf("scf = %d u_pol %e  rmsd %e \n",iscf,uupol*coul_unit,rmsd); 
+        }
+
+        iscf+=1;
+        printf("here 5\n");
+    }
+
+    *upol=uupol;
+    free(ef);
+    free(efg);
+    free(ef_stat);
+    free(ef_ind);
+    free(zeroch);
 
 }
+
+/******************************************************************************/
+/*  order of extrapolation is k+1 of Kolafa original derivation.              */
+/*  zero order is simply the previous step                                    */
+/*  if key == 1 => predictor                                                  */
+/*  if key == 2 => corrector                                                  */
+/******************************************************************************/
+void extrapolate_dipole_aspc(double (*mu_ind)[3] , double (*ef)[3], int key ){
+    //USE pim,              ONLY : extrapolate_order
+   
+    double B_ASPC[MAX_EXTRAPOLATE_ORDER+1], W_ASPC;
+    int ext_ord;
+
+    double (*mu_save)[3];
+    mu_save=malloc(nion*sizeof(*mu_save));
+
+
+    // switch to lower order of extrapolation
+    // if time step is not large enough to store previous dipoles steps
+    //
+    if ( istep >= extrapolate_order+1 ) {
+        ext_ord = extrapolate_order;
+    }
+    else {
+        if (istep <=1) {
+            ext_ord = 0;
+        }
+        else {
+            ext_ord = istep - 1;
+        }
+    }
+    printf("ext_ord %d\n",ext_ord);
+    switch (ext_ord) {
+        default:
+            io_node pError("value of aspc extrapolation order not available should be 0<= extrapolate_order <= 4\n");
+            exit(-1);
+            break;
+        case 0:
+            B_ASPC[0] = 1.0;
+            W_ASPC    = 0.0;
+            break;
+        case 1:
+            B_ASPC[0] =  2.0;
+            B_ASPC[1] = -1.0;
+            W_ASPC    =  2.0/3.0;
+            break;
+        case 2:
+            B_ASPC[0] =  2.5;
+            B_ASPC[1] = -2.0;
+            B_ASPC[2] =  0.5;
+            W_ASPC    =  0.6;
+            break;
+        case 3:
+            B_ASPC[0] =  2.8;
+            B_ASPC[1] = -2.8;
+            B_ASPC[2] =  1.2;
+            B_ASPC[3] = -0.2;
+            W_ASPC    =  4.0/7.0;
+            break;
+        case 4:
+            B_ASPC[0] =  3.0;
+            B_ASPC[1] = -24.0/7.0;
+            B_ASPC[2] =  27.0/14.0;
+            B_ASPC[3] = -4.0/7.0;
+            B_ASPC[4] =  1.0/14.0;
+            W_ASPC    =  5.0/9.0;
+            break;
+        case 5 :
+            B_ASPC[0] =  22.0/7.0;
+            B_ASPC[1] = -55.0/14.0;
+            B_ASPC[2] =  55.0/21.0;
+            B_ASPC[3] = -22.0/21.0;
+            B_ASPC[4] =  5.0/21.0;
+            B_ASPC[5] = -1.0/42.0;
+            W_ASPC    =  6.0/11.0;
+            break;
+    }
+    printf("after switch\n");
+
+    /* predictor */
+    if (key == 1) {
+        for (int ia=0;ia<nion;ia++){
+        //    printf("ia in predictor %d\n",ia);
+            for (int i=0;i<3;i++){
+                mu_ind[ia][i]=0.0;
+            }
+            for (int k=0;k<ext_ord+2;k++){
+                for (int i=0;i<3;i++){
+                    mu_ind[ia][i]+=B_ASPC[k]*dipia_ind[ia][k][i];
+                }
+            }
+            for (int k=ext_ord+1;k>3;k--){ 
+                for (int i=0;i<3;i++){
+                    dipia_ind[ia][k][i]=dipia_ind[ia][k-1][i];
+                }
+            }
+        }
+    } /* predictor */
+
+    /* corrector */
+    if (key == 2) {
+        /* -------------------------*/
+        /* save induced moment      */
+        for (int ia=0;ia<nion;ia++){
+            for (int i=0;i<3;i++){
+                mu_save[ia][i] = mu_ind[ia][i];
+            }
+        }
+        /* -------------------------*/
+        induced_moment(mu_ind,ef);
+        /* -------------------------*/
+        /* correction mu_ind = W mu_ind + (1-W) mu_ind */
+        for (int ia=0;ia<nion;ia++){
+            for (int i=0;i<3;i++){
+                mu_ind[ia][i] = mu_ind[ia][i]*W_ASPC + (1.0 - W_ASPC)*mu_save[ia][i];
+            }
+        }
+    }
+
+    free(mu_save);
+
+}
+/******************************************************************************/
+/* this subroutine calculates the induced moment from the total electric 
+   field and the polarizability tensor
+   Basically used in the SCF loop to get induced_moment.
+
+   \f$ \mu_{i,\alpha} =  p_{i,\alpha,\beta} * E_{i,\beta} \f$
+
+   in  :  Efield electric field vector define at ion position 
+   out : mu_ind induced electric dipole define at ion position 
+   polia is the polarizability tensor
+ ******************************************************************************/
+void induced_moment(double (*mu_ind)[3], double (*ef)[3]){
+
+    int it;
+    /* 
+    ---------------------------------------------------------------
+    \mu_{i,\alpha} =  alpha_{i,\alpha,\beta} * E_{i,\beta}
+    ---------------------------------------------------------------
+    note on units :
+    everything are in internal units :
+        [ Efield ] = e / A^2
+        [ polia  ] = A ^ 3
+        [ mu_ind ] = e A 
+    --------------------------------------------------------------- 
+    */
+    for (int ia=0;ia<nion;ia++){
+        it = typia[ia];
+        if (!lpolar[it]) continue;
+        for (int i=0;i<3;i++){
+            mu_ind[ia][i]=0.0;
+            for (int j=0;j<3;j++){
+                mu_ind[ia][i] += polia[ia][j][i]*ef[ia][j];
+            }
+        }
+    }
+}
+
+/******************************************************************************/
+/******************************************************************************/
+double get_rmsd_scf(double (*mu)[3], double (*ef)[3]){
+
+    double rmsd=0.0;
+    int npol=0;
+    int it;
+
+    for (int ia=0;ia<nion;ia++){
+        it = typia[ia];
+        if (!lpolar[it]) continue;
+        npol += 1;
+        for (int i=0;i<3;i++){
+            if (polia[ia][i][i] != 0.0 ){
+                rmsd+= (mu[ia][i] / polia[ia][i][i] - ef[ia][i])*
+                       (mu[ia][i] / polia[ia][i][i] - ef[ia][i]);
+            }
+        }
+    }
+    return sqrt(rmsd/(double)npol);
+}
+/*
+! *********************** SUBROUTINE get_rmsd_mu ****************************
+!> \brief
+! ***************************************************************************
+SUBROUTINE get_rmsd_mu ( rmsd , g0 , mu )
+
+  USE pim,              ONLY :  ldip_polar
+  USE config,           ONLY :  natm , itype , poldipia
+
+  implicit none
+  ! global
+  real(kind=dp) :: rmsd
+  real(kind=dp) :: g0(3,natm)
+  real(kind=dp) :: mu(3,natm)
+  ! local
+  integer :: alpha , ia ,  it
+  integer :: npol
+
+  rmsd = 0.0_dp
+  npol=0
+  do ia=1, natm
+    it = itype( ia)
+    if ( .not. ldip_polar( it ) ) cycle
+    npol = npol + 1
+    do alpha = 1 , 3
+      if ( poldipia ( alpha , alpha  , ia ) .eq. 0.0_dp ) cycle
+      rmsd  = rmsd  +  ( mu ( alpha , ia ) / poldipia ( alpha , alpha , ia ) - g0 ( alpha , ia ) ) ** 2.0_dp
+    enddo
+  enddo
+  rmsd = SQRT ( rmsd /  REAL(npol,kind=dp) )
+
+  return
+
+END SUBROUTINE get_rmsd_mu
+*/
+
+
--- src/pim.h
+++ src/pim.h
@@ -1,5 +1,11 @@
 #ifndef PIM_H
 #define PIM_H
+
+#define MAX_EXTRAPOLATE_ORDER 5
+int extrapolate_order;
+int min_scf_pim_iter;
+int max_scf_pim_iter;
+
 #include <stdbool.h>
 double conv_tol_ind;
 
@@ -10,8 +16,14 @@ int algo_pim;
 char allwd_algo_extrapolate_dipole[ALLWD_ALGO_EXTRAPOLATE_DIPOLE_STR][MAX_LEN+1];
 int algo_extrapolate_dipole;
 
-int extrapolate_order;
 
+/* ****************************************************************************/
+/*                                prototypes                                  */
+/* ****************************************************************************/
 void info_pim();
-void momentpolaSCF(double (*mu)[3]);
+void init_pim(char * controlfn);
+void momentpolaSCF(double (*mu)[3],double *upol);
+void induced_moment(double (*mu_ind)[3], double (*ef)[3]);
+void extrapolate_dipole_aspc(double (*mu_ind)[3] , double (*ef)[3], int key );
+double get_rmsd_scf(double (*mu)[3], double (*ef)[3]);
 #endif
