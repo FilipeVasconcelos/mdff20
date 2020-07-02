3a36c38b27bdb32d5bb33498fd46741a693ee43d
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-07-02 22:52:26 +0200

    ldamp bool variable to use damping in ewald_dir

--- src/ewald.h
+++ src/ewald.h
@@ -14,12 +14,12 @@ double  *thetaia;  /* quadrupole at ion */
 void set_autoES();
 
 void multipole_ES(double *q, double (*mu)[3], double (*theta)[3][3],double *u, double *pvir, double tau[3][3],
-                  double (*ef)[3], double (*efg)[3][3],bool lqch, bool ldip);
+                  double (*ef)[3], double (*efg)[3][3],bool lqch, bool ldip, bool ldamp);
 
 void multipole_ES_dir(double *q, double (*mu)[3], double (*theta)[3][3],
                       double *u_dir  , double (*ef_dir)[3], double (*efg_dir)[3][3], 
                       double *fx_dir, double *fy_dir, double *fz_dir , double tau_dir[3][3],
-                      bool lqch, bool ldip);
+                      bool lqch, bool ldip, bool ldamp);
 void multipole_ES_rec(double *q, double (*mu)[3], double (*theta)[3][3],
                       double *u_rec  , double (*ef_rec)[3], double (*efg_rec)[3][3],
                       double *fx_rec, double *fy_rec, double *fz_rec , double tau_rec[3][3], 
