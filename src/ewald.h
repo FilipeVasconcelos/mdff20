2aef517ba070659665f9f434974e1627a1741c9d
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-07-06 22:39:23 +0200

    tasks flags new prototypes

--- src/ewald.h
+++ src/ewald.h
@@ -14,12 +14,14 @@ double  *thetaia;  /* quadrupole at ion */
 void set_autoES();
 
 void multipole_ES(double *q, double (*mu)[3], double (*theta)[3][3],double *u, double *pvir, double tau[3][3],
-                  double (*ef)[3], double (*efg)[3][3],bool lqch, bool ldip, bool ldamp);
+                  double (*ef)[3], double (*efg)[3][3],bool lqch, bool ldip, bool lqua, bool ldamp,
+                  bool do_forces, bool do_stress, bool do_ef, bool do_efg, bool do_dir, bool do_rec);
 
 void multipole_ES_dir(double *q, double (*mu)[3], double (*theta)[3][3],
                       double *u_dir  , double (*ef_dir)[3], double (*efg_dir)[3][3], 
                       double *fx_dir, double *fy_dir, double *fz_dir , double tau_dir[3][3],
-                      bool lqch, bool ldip, bool ldamp);
+                      bool lqch, bool ldip, bool lqua, bool ldamp,
+                      bool do_forces, bool do_stress, bool do_ef, bool do_efg);
 void multipole_ES_rec(double *q, double (*mu)[3], double (*theta)[3][3],
                       double *u_rec  , double (*ef_rec)[3], double (*efg_rec)[3][3],
                       double *fx_rec, double *fy_rec, double *fz_rec , double tau_rec[3][3], 
