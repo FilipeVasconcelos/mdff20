ec5b073a7b5abf9ec401b010176d7c926e7fbaf3
Filipe Vasconcelos <filipe.vasconcelos@esme.fr>
2022-02-21 20:28:17 +0100

    scripts update

--- bin/fftocar.py
+++ bin/fftocar.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # ===============================================================
 # date       : 30 novembre 2016 
 # author     : fmv
@@ -13,12 +13,9 @@ from config import Ion
 def main_parser():
 
     parser = argparse.ArgumentParser(description='')
-
     parser.add_argument('-i','--input', help='Input file name',required=True,dest="input_file")
     parser.add_argument('-o','--output',help='Output file name',required=True,dest="output_file")
-
     args = parser.parse_args()
-
     return args
 
 
@@ -26,24 +23,17 @@ if __name__ == "__main__":
 
     separator=60*"="
     now = datetime.datetime.now()
-
     print separator
     print now.strftime("%Y-%m-%d %H:%M")
     print "author : filipe.manuel.vasconcelos@gmail.com"
     print separator
-    print "Running poszi ..."
+    print "Running fftocat ..."
     print "This script generate POSFF file from POSCAR file"
-
     args = main_parser()
-
     input_file=args.input_file
     output_file=args.output_file
-
     ions=[]
     conf_pos=Config(ions=ions)
-
     conf_pos.read_POSFF(input_file)
     conf_pos.write_POSCAR(output_file)
     print "file "+output_file+" generated"
-
-
